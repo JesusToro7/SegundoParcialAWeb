98e3b2a0fe506598890209fef23add2bdacd0b4b
Jesús Toro <102129327+JesusToro7@users.noreply.github.com>
2026-05-14 17:42:54 -0500

    Update config for environment variables

--- BookStore-MERN-stack/backend/config.js
+++ BookStore-MERN-stack/backend/config.js
@@ -2,4 +2,6 @@
 export const PORT = process.env.PORT || 5555;
 
 
-export const mongoDBURL = 'mongodb://localhost:27017/books-collection';
+//export const mongoDBURL = 'mongodb://localhost:27017/books-collection';
+
+export const mongoDBURL = process.env.MONGODB_URL || 'mongodb+srv://root:1234@books-store-mern.4bxfsw3.mongodb.net/books-collection?appName=Books-Store-MERN';
